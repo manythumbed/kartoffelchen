74a14de847d2f3db2196aba5102ca268a1d62469
Tim Clark <t.i.j.clark@gmail.com>
2013-02-10 10:32:32 +0000

    Pull transposition check into method on test data

--- notation_test.go
+++ notation_test.go
@@ -4,25 +4,29 @@ import "testing"
 
 func TestPitchTransposition(t *testing.T) {
 	for _, d := range transpositions {
-		if x := d.original.Transpose(d.amount); x != d.transposed {
-			t.Errorf("Expected pitch %v to be %v after transposition of %d, recieved %v", d.original, d.transposed, d.amount, x)
-		}
+		d.check(t)
 	}
 }
 
-type tdata struct {
+type transpositionCheck struct {
 	original, transposed Pitch
 	amount               int
 }
 
-var transpositions = []tdata{
-	tdata{Pitch{4, 0}, Pitch{4, 0}, 0},
-	tdata{Pitch{4, 0}, Pitch{4, 1}, 1},
-	tdata{Pitch{4, 0}, Pitch{5, 0}, 12},
-	tdata{Pitch{4, 0}, Pitch{3, 0}, -12},
-	tdata{Pitch{4, 0}, Pitch{3, 11}, -1},
-	tdata{Pitch{0, 0}, Pitch{-1, 11}, -1},
-	tdata{Pitch{0, 0}, Pitch{-1, 0}, -12},
-	tdata{Pitch{0, 0}, Pitch{-2, 11}, -13},
-	tdata{Pitch{-2, 11}, Pitch{-3, 10}, -13},
+func (c transpositionCheck) check(t *testing.T)	{
+	if x := c.original.Transpose(c.amount); x != c.transposed	{
+			t.Errorf("Expected pitch %v to be %v after transposition of %d, recieved %v", c.original, c.transposed, c.amount, x)
+	}
+}
+
+var transpositions = []transpositionCheck{
+	transpositionCheck{Pitch{4, 0}, Pitch{4, 0}, 0},
+	transpositionCheck{Pitch{4, 0}, Pitch{4, 1}, 1},
+	transpositionCheck{Pitch{4, 0}, Pitch{5, 0}, 12},
+	transpositionCheck{Pitch{4, 0}, Pitch{3, 0}, -12},
+	transpositionCheck{Pitch{4, 0}, Pitch{3, 11}, -1},
+	transpositionCheck{Pitch{0, 0}, Pitch{-1, 11}, -1},
+	transpositionCheck{Pitch{0, 0}, Pitch{-1, 0}, -12},
+	transpositionCheck{Pitch{0, 0}, Pitch{-2, 11}, -13},
+	transpositionCheck{Pitch{-2, 11}, Pitch{-3, 10}, -13},
 }
