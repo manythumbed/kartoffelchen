8b1820d0176040b627d4bde87c2599bb263b1a79
Tim Clark <t.i.j.clark@gmail.com>
2013-02-10 21:09:14 +0000

    Start of combining music

--- notation.go
+++ notation.go
@@ -20,3 +20,81 @@ func (p Pitch) Transpose(semitones int) Pitch {
 	}
 	return Pitch{Octave(t / 12), NoteIndex(t % 12)}
 }
+
+type Duration int
+
+type Note struct {
+	Pitch
+	Duration
+}
+
+func (n Note) Transpose(semitones int) Note {
+	return Note{n.Pitch.Transpose(semitones), n.Duration}
+}
+
+type Rest struct {
+	Duration
+}
+
+func (r Rest) Transpose(semitones int) Rest {
+	return Rest{r.Duration}
+}
+
+type MusicalEvent interface {
+	Pitched() bool
+	GetPitch() *Pitch
+	GetDuration() Duration
+}
+
+func (r Rest) Pitched() bool	{
+	return false
+}
+
+func (r Rest) GetPitch() *Pitch	{
+	return nil
+}
+
+func (r Rest) GetDuration() Duration	{
+	return r.Duration
+}
+
+/*
+func (n Note) Pitched() bool {
+	return true
+}
+
+func (n Note) GetPitch() Pitch {
+	return &n.Pitch
+}
+
+func (n Note) GetDuration() Duration {
+	return n.Duration
+}
+*/
+
+/*
+	Music is a sequence of musical events, sequential composition
+	or parallel composition
+*/
+type Sequential []MusicalEvent
+type Parallel []MusicalEvent
+
+type Music struct	{
+	music []interface{}
+}
+
+func (m *Music) AddNote(e Note)	{
+	m.music = append(m.music, e)
+}
+
+func (m *Music) AddRest(r Rest)	{
+	m.music = append(m.music, r)
+}
+
+func (m *Music) Add(music Music)	{
+	m.music = append(m.music, music.music...)
+}
+
+func (m *Music) Combine(music Music)	{
+	m.music = []interface{}{m.music, music.music}
+}
--- notation_test.go
+++ notation_test.go
@@ -13,9 +13,9 @@ type transpositionCheck struct {
 	amount               int
 }
 
-func (c transpositionCheck) check(t *testing.T)	{
-	if x := c.original.Transpose(c.amount); x != c.transposed	{
-			t.Errorf("Expected pitch %v to be %v after transposition of %d, recieved %v", c.original, c.transposed, c.amount, x)
+func (c transpositionCheck) check(t *testing.T) {
+	if x := c.original.Transpose(c.amount); x != c.transposed {
+		t.Errorf("Expected pitch %v to be %v after transposition of %d, recieved %v", c.original, c.transposed, c.amount, x)
 	}
 }
 
@@ -29,4 +29,16 @@ var transpositions = []transpositionCheck{
 	transpositionCheck{Pitch{0, 0}, Pitch{-1, 0}, -12},
 	transpositionCheck{Pitch{0, 0}, Pitch{-2, 11}, -13},
 	transpositionCheck{Pitch{-2, 11}, Pitch{-3, 10}, -13},
+}
+
+func TestMusic(t * testing.T)	{
+	m := Music{}
+	m.AddNote(Note{Pitch{4, 0}, 1})
+	m.AddRest(Rest{1})
+
+	n := Music{}
+	n.AddRest(Rest{1})
+	m.Add(n)
+	m.Combine(n)
+	t.Errorf("%v", m)
 }
