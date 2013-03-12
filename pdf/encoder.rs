3b3b27ebdd6293400c5bedd1f6311197761994da
Tim Clark <t.i.j.clark@gmail.com>
2013-03-12 18:41:30 +0000

    More string generation

--- pdf/encoder.rs
+++ pdf/encoder.rs
@@ -11,10 +11,20 @@ fn header() -> ~str {
 	~"%PDF-1.7" + newline + ~"%\x93\x8c\x8b\x9e" + newline 
 }
 
+fn trailerHeader() -> ~str {
+	~"trailer" + newline
+}
+
+fn eof() -> ~str	{
+	~"%%EOF" + newline
+}
+
 #[test]
 fn test_output()	{
 	let result = chain(file_writer(&path::Path("test.pdf"), &[Create]),	|w| { 
-		w.write_str("abc");
+		w.write_str(header());
+		w.write_str(trailerHeader());
+		w.write_str(eof());
 		Ok("done") 
 	});
 	
