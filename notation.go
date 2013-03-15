ca6cca438f312d803d65b8de265d2258833172cd
Tim Clark <t.i.j.clark@gmail.com>
2013-03-15 21:58:31 +0000

    Pull in rational implementation and run gofmt

--- notation.go
+++ notation.go
@@ -4,7 +4,7 @@ type Pitch int
 type Duration int
 type Position int
 
-type Primitive interface	{
+type Primitive interface {
 	Pitch() (bool, Pitch)
 	Length() (bool, Duration)
 }
@@ -14,42 +14,42 @@ type Event struct {
 	Position
 }
 
-type Rest struct	{
+type Rest struct {
 	duration Duration
 }
 
-func (r Rest) Pitch() (bool, Pitch)	{
+func (r Rest) Pitch() (bool, Pitch) {
 	return false, Pitch(0)
 }
 
-func (r Rest) Length() (bool, Duration)	{
+func (r Rest) Length() (bool, Duration) {
 	return true, r.duration
 }
 
-type Note struct	{
-	pitch Pitch
+type Note struct {
+	pitch    Pitch
 	duration Duration
 }
 
-func (n Note) Pitch() (bool, Pitch)	{
+func (n Note) Pitch() (bool, Pitch) {
 	return true, n.pitch
 }
 
-func (n Note) Length() (bool, Duration)	{
+func (n Note) Length() (bool, Duration) {
 	return true, n.duration
 }
 
-func position(initial Position, duration Duration) Position	{
+func position(initial Position, duration Duration) Position {
 	return Position(int(initial) + int(duration))
 }
 
-func Events(notes []Primitive) []Event	{
+func Events(notes []Primitive) []Event {
 	events := make([]Event, len(notes))
 	p := Position(0)
 
-	for i, n := range notes	{
+	for i, n := range notes {
 		events[i] = Event{n, p}
-		if ok, dur := n.Length(); ok	{
+		if ok, dur := n.Length(); ok {
 			p = position(p, dur)
 		}
 	}
--- notation_test.go
+++ notation_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestRest(t *testing.T)	{
+func TestRest(t *testing.T) {
 	r := Rest{1}
 
 	l, d := r.Length()
@@ -12,16 +12,16 @@ func TestRest(t *testing.T)	{
 		t.Errorf("A rest has length")
 	}
 
-	if d != 1	{
+	if d != 1 {
 		t.Errorf("Rest length should be 1, was given %v", d)
 	}
 
-	if p,_ := r.Pitch(); p != false	{
+	if p, _ := r.Pitch(); p != false {
 		t.Errorf("A rest has no pitch")
 	}
 }
 
-func TestNote(t *testing.T)	{
+func TestNote(t *testing.T) {
 	n := Note{1, 2}
 
 	l, d := n.Length()
@@ -29,14 +29,14 @@ func TestNote(t *testing.T)	{
 		t.Errorf("A note has length")
 	}
 
-	if d != 2	{
+	if d != 2 {
 		t.Errorf("Note length should be 2, was given %v", d)
 	}
 
 }
 
-func TestEvents(t *testing.T)	{
-	if len(Events([]Primitive{})) != 0	{
+func TestEvents(t *testing.T) {
+	if len(Events([]Primitive{})) != 0 {
 		t.Errorf("An empty slice of primitives should produce an empty slice of events")
 	}
 
@@ -45,13 +45,13 @@ func TestEvents(t *testing.T)	{
 		t.Errorf("Expected 3 events, received %d", len(e))
 	}
 
-	if e[0].Position != 0	{
+	if e[0].Position != 0 {
 		t.Errorf("Expected position of 0, received %v", e[0].Position)
 	}
-	if e[1].Position != 1	{
+	if e[1].Position != 1 {
 		t.Errorf("Expected position of 1, received %v", e[1].Position)
 	}
-	if e[2].Position != 3	{
+	if e[2].Position != 3 {
 		t.Errorf("Expected position of 3, received %v", e[2].Position)
 	}
 }
