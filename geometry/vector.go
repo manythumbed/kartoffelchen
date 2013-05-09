3cb4ca01e96570a19d4e807b1706c79bff185b89
Tim Clark <t.i.j.clark@gmail.com>
2013-05-09 21:07:43 +0100

    More simple 2d vector functions

--- geometry/vector.go
+++ geometry/vector.go
@@ -1,5 +1,26 @@
 package geometry
 
+import (
+	"math"
+)
+
 func mediate(t float64, a, b complex128) complex128 {
 	return a + complex(t, 0)*(b-a)
+}
+
+func normals(a, b complex128) (complex128, complex128) {
+	dx := real(b) - real(a)
+	dy := imag(b) - imag(a)
+
+	n1 := complex(-1*dy, dx)
+	n2 := complex(dy, -1*dx)
+
+	return n1, n2
+}
+
+func magnitude(a, b complex128) float64 {
+	dx := real(b) - real(a)
+	dy := imag(b) - imag(a)
+
+	return math.Sqrt((dx * dx) + (dy * dy))
 }
--- geometry/vector_test.go
+++ geometry/vector_test.go
@@ -1,11 +1,26 @@
 package geometry
 
 import (
+	"math"
 	"testing"
 )
 
 func TestMediate(t *testing.T) {
 	if mediate(0.5, complex(0, 0), complex(1, 1)) != complex(0.5, 0.5) {
 		t.Errorf("Mediate produced wrong point")
+	}
+}
+
+func TestNormals(t *testing.T) {
+	n1, n2 := normals(complex(0, 0), complex(1, 1))
+	if n1 != complex(-1, 1) || n2 != complex(1, -1) {
+		t.Errorf("Expected (-1+1j) and (1-1j) got %v %v", n1, n2)
+	}
+}
+
+func TestMagnitude(t *testing.T) {
+	m := magnitude(complex(0, 0), complex(1, 1))
+	if m != math.Sqrt(2) {
+		t.Errorf("Expected square root of 2, received %v", m)
 	}
 }
